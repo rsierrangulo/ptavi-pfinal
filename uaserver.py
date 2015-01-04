3d13afa045f3561c3de6ab7a36875a658d90ef69
rsierrangulo <rsierrangulo@gmail.com>
2015-01-04 13:14:19 +0100

    uaserver con envios

--- uaserver.py
+++ uaserver.py
@@ -3,6 +3,7 @@
 
 
 import SocketServer
+import socket
 import sys
 import os
 from xml.sax import make_parser
@@ -65,26 +66,28 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
         """
         Método handle
         """
-        CLIENT = str(self.client_address[0])
         while 1:
             line = self.rfile.read()
             if not line:
                 break
-            print "El cliente nos manda " + line
+            print "RECIBIDO ", line
             lista = line.split(" ")
 
+            metodo = lista[0]            
             metodos = ['INVITE', 'ACK', 'BYE']
 
             if metodo == "INVITE":
                 respuesta = "SIP/2.0 100 Trying\r\n\r\n"
                 respuesta += "SIP/2.0 180 Ringing\r\n\r\n"
-                respuesta += "SIP/2.0 200 OK\r\n\r\n"
+                respuesta += "SIP/2.0 200 OK\r\n"
                 respuesta += "Content-Type: application/sdp\r\n"
                 respuesta += "v=0\r\n"
                 respuesta += "o=" + usuario + " " + uaip + "\r\n"
                 respuesta += "s=misesion\r\n"
                 respuesta += "t=0\r\n"
                 respuesta += "m=audio8 " + audioport + " RTP\r\n\r\n"
+                self.wfile.write(respuesta)
+                print respuesta
             elif metodo == "ACK":
                 lista_split = lista[4].split("\r\n")
                 ip_recibe = lista_split[0]
@@ -96,18 +99,12 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 print(" Hemos terminado la ejecución de fichero de audio")
             elif metodo == "BYE":
                 self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
-                my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                my_socket.connect((proxyip, proxyport))
             elif not metodo in metodos:
                 self.wfile.write("SIP/2.0 405 Method Not Allowed\r\n\r\n")
             else:
                 self.wfile.write("SIP/2.0 400 Bad Request\r\n\r\n")
 
-        my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        my_socket.connect((proxyip, proxyport))
-        my_socket.send(line)
+
 
 if __name__ == "__main__":
     """
